607efa03bafa084980be9a807e374a2dc2d871d9
Anton Tayanovskyy <anton@pulumi.com>
2021-06-16 17:05:23 -0400

    Prettier metric names, derived metrics

---
+++ traces/metric_names.go
@@ -0,0 +1,51 @@
+package traces
+
+const benchmark_name string = "benchmark_name"
+const time_total_ms string = "time_total_ms"
+const pulumi_api = "pulumi_api"
+
+const time_log_overhead_ms = "time_log_overhead_ms"
+const time_pulumi_api_ms = "time_pulumi_api_ms"
+const time_to_engine_ms = "time_to_engine_ms"
+
+// Duration of the span marked `pulumi-plan`. This span seem to cover
+// plan and/or update operations.
+const time_engine_ms string = "time_engine_ms"
+
+// Phases include things like pulumi-update-initial as defined by ProgramTest.
+const benchmark_phase string = "benchmark_phase"
+
+// Process such as `pulumi` or `pulumi-resource-aws` the data is coming from.
+const pulumi_process string = "pulumi_process"
+
+// Maps canonical column names as they should appear in `merics.csv`
+// to a list of possible aliases how they appear in the `traces.csv`
+// files and binary traces.
+func metricAliases() map[string][]string {
+	return map[string][]string{
+		benchmark_name:          {"benchmark_name"},
+		"benchmark_provider":    {"benchmark_provider", "benchmark_cloud"},
+		"benchmark_repo":        {"repo"},
+		"benchmark_runtime":     {"benchmark_language", "benchmark_runtime"},
+		"go_num_cgo_call":       {"runtime.NumCgoCall"},
+		"mem_frees":             {"MemStats.Frees"},
+		"mem_heap_alloc_max":    {"MemStats.HeapAlloc.Max"},
+		"mem_heap_idle_max":     {"MemStats.HeapIdle.Max"},
+		"mem_heap_inuse_max":    {"MemStats.HeapInuse.Max"},
+		"mem_heap_objects_max":  {"MemStats.HeapObjects.Max"},
+		"mem_heap_released_max": {"MemStats.HeapReleased.Max"},
+		"mem_heap_sys_max":      {"MemStats.HeapSys.Max"},
+		"mem_mallocs":           {"MemStats.Mallocs"},
+		"mem_num_gc":            {"MemStats.NumGC"},
+		"mem_pause_total_ns":    {"MemStats.PauseTotalNs"},
+		"mem_stack_in_use_max":  {"MemStats.StackInuse.Max"},
+		"mem_stack_sys_max":     {"MemStats.StackSys.Max"},
+		"mem_sys_max":           {"MemStats.Sys.Max"},
+		"mem_total_alloc":       {"MemStats.TotalAlloc"},
+		"pulumi_version":        {"pulumi_version"},
+		"pulumi_commandline":    {"os.Args"},
+		"runner_arch":           {"runtime.GOARCH"},
+		"runner_num_cpu":        {"runtime.NumCPU"},
+		"runner_os":             {"runtime.GOOS"},
+	}
+}
--- traces/metrics.go
+++ traces/metrics.go
@@ -11,100 +11,239 @@ import (
 )
 
 func Metrics(csvFile string, filenameColumn string, writer io.Writer) error {
+	aliases := metricAliases()
 
+	invAliases := make(map[string]string)
+	for k, vs := range aliases {
+		for _, v := range vs {
+			invAliases[v] = k
+		}
+	}
+
+	var metrics []map[string]string
+
+	var logOverhead, apiOverhead, engDuration time.Duration
+	var engStart time.Time
+	var pulumiApiEndpoint string
+
+	// First pass: precompute some metrics
+	err := readLargeCsvFile(csvFile, func(row map[string]string) error {
+
+		if row["Name"] == "/pulumirpc.Engine/Log" {
+			dur, err := spanDuration(row)
+			if err != nil {
+				return err
+			}
+			logOverhead += dur
+		}
+
+		if row["Name"] == "pulumi-plan" {
+			t0, err := spanStart(row)
+			if err != nil {
+				return err
+			}
+			engStart = t0
+
+			dur, err := spanDuration(row)
+			if err != nil {
+				return err
+			}
+			engDuration = dur
+		}
+
+		if row["api"] != "" {
+			pulumiApiEndpoint = row["api"]
+			dur, err := spanDuration(row)
+			if err != nil {
+				return err
+			}
+			apiOverhead += dur
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// total_time_ms = df[df['Name'] == 'pulumi']['time_ms'].iloc[0]
+	// plan_time_ms = df[df['Name'] == 'pulumi-plan']['time_ms'].iloc[0]
+
+	// api_ms = df[df['api'] == 'https://api.pulumi.com']['time_ms'].sum()
+	// log_ms = df[df['Name'] == '/pulumirpc.Engine/Log']['time_ms'].sum()
+
+	// Second pass: collect metrics
+	err = readLargeCsvFile(csvFile, func(row map[string]string) error {
+
+		// Detect the all-encompassing span collected from the
+		// top-level `pulumi` invocation.
+
+		if row["Name"] == "pulumi" {
+			m := make(map[string]string)
+
+			t0, err := spanStart(row)
+			if err != nil {
+				return err
+			}
+
+			// this is coming from `pulumi` CLI process, not a plugin
+			m[pulumi_process] = "pulumi"
+
+			// compute duration
+			dur, err := spanDuration(row)
+			if err != nil {
+				return err
+			}
+			m[time_total_ms] = ms(dur)
+
+			// copy labels if found in aliases
+			for k, v := range row {
+				col, includeCol := invAliases[k]
+				if includeCol {
+					m[col] = v
+				}
+			}
+
+			// infer benchmark phase; example inputs:
+			//
+			// filename=aws-go-s3-folder-pulumi-update-initial.trace
+			// benchmark_name=aws-go-s3-folder
+			m[benchmark_phase] = ""
+			if strings.HasPrefix(row[filenameColumn], m[benchmark_name]+"-") {
+				s := strings.TrimPrefix(row[filenameColumn], m[benchmark_name]+"-")
+				if strings.HasSuffix(s, ".trace") {
+					s = strings.TrimSuffix(s, ".trace")
+					m[benchmark_phase] = s
+				}
+			}
+
+			// use pre-computed things here
+			m[time_engine_ms] = ms(engDuration)
+			m[pulumi_api] = pulumiApiEndpoint
+			m[time_pulumi_api_ms] = ms(apiOverhead)
+			m[time_log_overhead_ms] = ms(logOverhead)
+			m[time_to_engine_ms] = ms(engStart.Sub(t0))
+
+			metrics = append(metrics, m)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if err := writeSmallCsvFile(metrics, writer); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func spanStart(row map[string]string) (time.Time, error) {
+	spanStart, err := parseTime(row["Span.Start"])
+	if err != nil {
+		return time.Time{}, err
+	}
+	return spanStart, nil
+}
+
+func spanDuration(row map[string]string) (time.Duration, error) {
+	spanStart, err := spanStart(row)
+	if err != nil {
+		return 0, err
+	}
+	spanEnd, err := parseTime(row["Span.End"])
+	if err != nil {
+		return 0, err
+
+	}
+	dur := spanEnd.Sub(spanStart)
+	return dur, nil
+
+}
+
+func readLargeCsvFile(csvFile string, handleRow func(map[string]string) error) error {
 	f, err := os.Open(csvFile)
 	if err != nil {
 		return err
 	}
-
 	defer f.Close()
 
-	r := bufio.NewReader(f)
+	reader := bufio.NewReader(f)
+	csvReader := csv.NewReader(reader)
 
-	cr := csv.NewReader(r)
-
-	header, err := cr.Read()
+	header, err := csvReader.Read()
 	if err != nil {
 		return err
 	}
 
-	inColumns := make(map[string]int)
-	outColumns := make(map[string]int)
-
-	addInColumn := func(name string) {
-		n := len(inColumns)
-		inColumns[name] = n
-	}
-
-	addOutColumn := func(name string) {
-		n := len(outColumns)
-		outColumns[name] = n
-	}
-
-	addOutColumn(filenameColumn)
-	addOutColumn("time_ms")
-
-	cw := csv.NewWriter(writer)
-
-	for _, h := range header {
-		addInColumn(h)
-
-		if strings.HasPrefix(h, "MemStats.") ||
-			strings.HasPrefix(h, "benchmark_") ||
-			strings.HasPrefix(h, "runtime.") ||
-			h == "repo" ||
-			h == "os.Args" ||
-			h == "Span.Start" ||
-			h == "Span.End" {
-
-			addOutColumn(h)
-		}
-	}
-
-	outColumnNames := make([]string, len(outColumns))
-	for k, j := range outColumns {
-		outColumnNames[j] = k
-	}
-
-	if err := cw.Write(outColumnNames); err != nil {
-		return err
-	}
-	cw.Flush()
-
 	for {
-		record, err := cr.Read()
+		record, err := csvReader.Read()
+
 		if err == io.EOF {
 			break
 		}
+
 		if err != nil {
 			return err
 		}
 
-		if record[inColumns["Name"]] == "pulumi" {
+		values := make(map[string]string, len(header))
 
-			values := make([]string, len(outColumns))
-
-			for k, j := range outColumns {
-				values[j] = record[inColumns[k]]
-			}
-
-			spanStart, err := parseTime(values[outColumns["Span.Start"]])
-			if err != nil {
-				return err
-			}
-			spanEnd, err := parseTime(values[outColumns["Span.End"]])
-			if err != nil {
-				return err
-			}
-			dur := spanEnd.Sub(spanStart)
-
-			values[outColumns["time_ms"]] = fmt.Sprintf("%v",
-				int64(dur/time.Millisecond))
-
-			cw.Write(values)
-			cw.Flush()
+		for i, value := range record {
+			values[header[i]] = value
 		}
 
+		if err := handleRow(values); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeSmallCsvFile(data []map[string]string, writer io.Writer) error {
+	csvWriter := csv.NewWriter(writer)
+	defer csvWriter.Flush()
+
+	columns := make(map[string]int)
+
+	addColumn := func(name string) {
+		_, seen := columns[name]
+		if !seen {
+			n := len(columns)
+			columns[name] = n
+		}
+	}
+
+	for _, row := range data {
+		for k := range row {
+			addColumn(k)
+		}
+	}
+
+	columnNames := make([]string, len(columns))
+
+	for k, j := range columns {
+		columnNames[j] = k
+	}
+
+	if err := csvWriter.Write(columnNames); err != nil {
+		return err
+	}
+
+	for _, row := range data {
+		values := make([]string, len(columns))
+
+		for k, v := range row {
+			values[columns[k]] = v
+		}
+
+		if err := csvWriter.Write(values); err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -112,4 +251,8 @@ func Metrics(csvFile string, filenameColumn string, writer io.Writer) error {
 
 func parseTime(str string) (time.Time, error) {
 	return time.Parse(time.RFC3339, str)
+}
+
+func ms(dur time.Duration) string {
+	return fmt.Sprintf("%v", int64(dur/time.Millisecond))
 }
